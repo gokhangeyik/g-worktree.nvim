96e198f7e12bb63b4459295db84e9c59a02073b9
Gokhan Geyik <contact@gokhangeyik.net>
2024-10-03 14:07:08 +0200

    WIP

--- lua/g-worktree/init.lua
+++ lua/g-worktree/init.lua
@@ -25,6 +25,7 @@ local function _resolve_wt_path(branch_name)
 		absolute_git_dir = stdout[1]
 	end
 	local git_dir_path = Path:new(absolute_git_dir):absolute()
+	vim.print("HERE")
 	vim.print(git_dir_path)
 	local git_dir_name = string.match(git_dir_path, "([^/]+)/.git/?[^/]*")
 	local git_dir_base_path = string.match(git_dir_path, "(.+)/.git")
